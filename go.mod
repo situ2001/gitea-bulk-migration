1f704c5c13502e8b3fa12c99ad805119fb68961c
situ2001 <yongcong2001@outlook.com>
2025-04-14 22:55:51 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github/situ2001.com/gitea-bulk-migration
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	n, err := fmt.Println("Hello, World!")
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Number of bytes written:", n)
+	}
+}
